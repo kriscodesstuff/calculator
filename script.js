7b33e5fbf92b4c7a3f2b1be1689c675972326ed1
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-07-31 12:38:24 -0500

    Add event listener conditionals

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const screen = document.querySelector('#screen');
 const calculatorItems = document.querySelectorAll('.calculator-item');
 
+
+
 const add = function(a,b){
     console.log(a + b);
     return a + b;
@@ -21,9 +23,11 @@ const divide = function(a,b){
     return a / b;
 }
 
-let num1;
+
+
+let num1 = '';
 let operator;
-let num2;
+let num2 = '';
 
 const operate = function(num1,num2,operator){
     switch(operator){
@@ -40,6 +44,38 @@ const operate = function(num1,num2,operator){
             divide(num1,num2);
     }
 }
+
+
+
+const getResults = function(e){
+
+    if(operator){
+        num2 += e.target.id;
+        screen.innerText = num2;
+        console.log(num1,num2);
+    }
+
+    if(!operator){
+
+        if(e.target.id == '1' || e.target.id == '2' || e.target.id == '3' || e.target.id == '4'
+        || e.target.id == '5' || e.target.id == '6' || e.target.id == '7' || e.target.id == '8'
+        ||e.target.id == '9'){
+            num1 += e.target.id;
+            screen.innerText = num1;
+        }else if(e.target.id == 'add-btn' || e.target.id == 'subtract-btn' ||
+        e.target.id == 'multiply-btn' || e.target.id == 'divide-btn'){
+            operator = e.target.id;
+        }
+    }
+    
+
+  
+}
+
+calculatorItems.forEach(item =>{
+    item.addEventListener('click', getResults)
+})
+
 
 
 
